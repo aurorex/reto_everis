5e8b99355a8005518b03b6584c35f132eda675d9
valeriavalles <valeria.sv@outlook.es>
2018-06-14 00:23:26 -0500

    probando router con angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
+// Rutas 
+import { RouterModule, Route } from '@angular/router'
+
 import { NgModule } from '@angular/core';
+
 
 import { FormsModule } from '@angular/forms'
 
@@ -20,7 +24,15 @@ import { ProductListComponent } from './components/publication/public-list/produ
 import { ProductComponent } from './components/publication/publications/product.component';
 
 // Services 
-import { ProductService } from './services/project.service'
+import { ProductService } from './services/project.service';
+import { HomeComponent } from './singlepage/home/home.component';
+import { LoguinComponent } from './singlepage/loguin/loguin.component';
+import { RegisterComponent } from './singlepage/register/register.component'
+// import { Route } from '@angular/compiler/src/core';
+
+const routeConfig:Route[] = [
+ 
+];
 
 @NgModule({
   declarations: [
@@ -28,10 +40,14 @@ import { ProductService } from './services/project.service'
     MainComponent,
     ProductsComponent,
     ProductListComponent,
-    ProductComponent
+    ProductComponent,
+    HomeComponent,
+    LoguinComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
     FormsModule,
